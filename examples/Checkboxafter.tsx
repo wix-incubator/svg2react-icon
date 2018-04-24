a6c5e8cf9eae54f6d07a506106cad3f207c32e35
Vytautas Jakutis <jakutis@wix.com>
2018-04-24 11:48:30 +0300

    add examples

---
+++ examples/Checkboxafter.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+/* tslint:disable */
+import * as React from 'react';
+import Icon, { IconProps } from '../Icon';
+const Checkboxafter: React.SFC<IconProps> = props => (
+  <Icon viewBox="0 0 30 30" {...props}>
+    <circle cx="15" cy="15" r="15" />
+    <path color="#FFF" fill="#FFF" d="M20.206 10l-5.386 6.824-2.757-2.801L11 15.22l3.942 4.01 6.443-8.164z" />
+  </Icon>
+);
+export default Checkboxafter;
+/* tslint:enable */
+/* eslint-enable */
