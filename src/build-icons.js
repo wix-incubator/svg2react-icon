e7bbd6d21e67ce51fa52a8fe2d39307ccbc87a6a
Felix Mosheev <felixm@wix.com>
2018-07-12 21:14:28 +0300

    Add named export support

--- src/build-icons.js
+++ src/build-icons.js
@@ -6,35 +6,35 @@ const optimizeSvg = require('./lib/svg-optimizer');
 
 const componentsDirName = 'components';
 
-module.exports = ({inputDir, outputDir, typescript, monochrome}) => {
+module.exports = ({inputDir, outputDir, typescript, monochrome, namedExport}) => {
   if (!inputDir || !outputDir) {
     throw new Error('Input and output dirs not specified');
   }
   const icons = glob.sync(`${inputDir}/**/*.svg`);
-  return processIcons(icons, outputDir, typescript, monochrome);
+  return processIcons(icons, outputDir, {isTypeScriptOutput: typescript, monochrome, namedExport});
 };
 
-async function processIcons(filenames, outputDir, isTypeScriptOutput, monochrome) {
+async function processIcons(filenames, outputDir, options) {
   fs.removeSync(outputDir);
   fs.mkdirsSync(path.join(outputDir, componentsDirName));
 
   const componentNames = await Promise.all(
-    filenames.map(icon => processIcon(icon, outputDir, isTypeScriptOutput, monochrome))
+    filenames.map(icon => processIcon(icon, outputDir, options))
   );
 
-  createIndexFile(componentNames, outputDir, isTypeScriptOutput);
+  createIndexFile(componentNames, outputDir, options);
 }
 
-async function processIcon(svgPath, outputDir, isTypeScriptOutput, monochrome) {
+async function processIcon(svgPath, outputDir, options) {
   const name = path.basename(svgPath, '.svg');
-  const filename = name + (isTypeScriptOutput ? '.tsx' : '.js');
+  const filename = name + (options.isTypeScriptOutput ? '.tsx' : '.js');
   const relativePath = path.join(componentsDirName, filename);
   const absolutePath = path.join(outputDir, relativePath);
 
   try {
     const svg = fs.readFileSync(svgPath, 'utf-8');
-    const optimizedSvg = await optimizeSvg(svg, monochrome);
-    const componentCode = svgToComponent(name, optimizedSvg, isTypeScriptOutput);
+    const optimizedSvg = await optimizeSvg(svg, options.monochrome);
+    const componentCode = svgToComponent(name, optimizedSvg, options);
     fs.writeFileSync(absolutePath, componentCode, 'utf-8');
     console.log(`Created: ${relativePath}`);
   } catch (err) {
@@ -44,12 +44,12 @@ async function processIcon(svgPath, outputDir, isTypeScriptOutput, monochrome) {
   return name;
 }
 
-function createIndexFile(componentNames, outputDir, isTypeScriptOutput) {
+function createIndexFile(componentNames, outputDir, options) {
   const code = componentNames.map(name =>
-    `export {default as ${name}} from './${componentsDirName}/${name}';`
+    `export {${options.namedExport ? '' : 'default as '}${name}} from './${componentsDirName}/${name}';`
   ).join('\n') + '\n';
 
-  const filename = 'index' + (isTypeScriptOutput ? '.ts' : '.js');
+  const filename = 'index' + (options.isTypeScriptOutput ? '.ts' : '.js');
   fs.writeFileSync(path.join(outputDir, filename), code, 'utf-8');
   console.log(`Created: ${filename}`);
 }
--- src/lib/parse-params.js
+++ src/lib/parse-params.js
@@ -4,11 +4,13 @@ program
   .arguments('<input-dir> <output-dir>')
   .option('--monochrome', 'Strip colors from the SVG')
   .option('--typescript', 'Produce TypeScript output')
+  .option('--named-export', 'Produce named export')
   .parse(process.argv);
 
 module.exports = {
   inputDir: program.args[0],
   outputDir: program.args[1],
   monochrome: Boolean(program.monochrome),
-  typescript: Boolean(program.typescript)
+  typescript: Boolean(program.typescript),
+  namedExport: Boolean(program.namedExport)
 };
